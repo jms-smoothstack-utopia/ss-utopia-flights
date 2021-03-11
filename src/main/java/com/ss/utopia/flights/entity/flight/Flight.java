6a09b964f64e44bbbf55c89a0fe2d6637835ee4d
Stephen Gerkin <stephen.gerkin@smoothstack.com>
2021-03-11 10:37:57 -0500

    HOTFIX: Fetch flight seats eagerly for now

--- src/main/java/com/ss/utopia/flights/entity/flight/Flight.java
+++ src/main/java/com/ss/utopia/flights/entity/flight/Flight.java
@@ -7,6 +7,7 @@ import java.util.List;
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
@@ -41,7 +42,7 @@ public class Flight {
   @OneToOne
   private Airplane airplane;
 
-  @OneToMany(cascade = CascadeType.REMOVE)
+  @OneToMany(cascade = CascadeType.REMOVE, fetch = FetchType.EAGER)
   private List<Seat> seats;
 
   @Column(updatable = false)
